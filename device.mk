fb939c0346eac902d45bfbd5ef20bc09fcc2afc0
tarsin <yuanqingxiang233@163.com>
2022-08-19 23:10:55 +0200

    apollo: Add shim for missing symbol in com.mi.node.watermark.so

--- device.mk
+++ device.mk
@@ -60,6 +60,10 @@ PRODUCT_COPY_FILES += \
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 29
 
+# Shims
+PRODUCT_PACKAGES += \
+    lib-watermarkshim
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
