672b5bece7edc45cbc400db8b929f86bba3c9ce3
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-11-24 23:54:45 +0100

    apollon: Import QDCM calibration config
    Change-Id: Ifc8d7c4bab926f34b9f1ce154f05b6408ed1fc27

--- device.mk
+++ device.mk
@@ -38,6 +38,10 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_PACKAGES += \
     lib-watermarkshim
 
+# QDCM
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/qdcm/,$(TARGET_COPY_OUT_VENDOR)/etc)
+
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 29
 
