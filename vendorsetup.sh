f09d3ee69d58eabd7ec8d6c91f0ff9b63879c9ea
NutellaDev <eduardocayovilca@gmail.com>
2023-08-16 10:08:06 -0500

    apollo: Update Clang to Sdclang

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,13 +1,17 @@
 echo 'Starting to clone stuffs needed for your device'
-echo 'Cloning Common Tree [1/3]'
+echo 'Cloning Common Tree [1/4]'
 # Sweet-Common Tree
 git clone https://github.com/NutellaSimpDev/device_xiaomi_sm8250-common.git -b skyline device/xiaomi/sm8250-common
 
-echo 'Cloning Vendor tree [2/3]'
+echo 'Cloning Vendor tree [2/4]'
 # Device Vendor Tree
 git clone https://github.com/NutellaSimpDev/vendor_xiaomi_apollo.git -b skyline vendor/xiaomi
 
-echo 'Cloning Kernel tree [3/3]'
+echo 'Cloning Kernel tree [3/4]'
 # Kernel Tree
 git clone https://github.com/VoidUI-Devices/kernel_xiaomi_sm8250.git -b aosp-13 kernel/xiaomi/sm8250
+
+echo 'Cloning Clang [4/4]'
+# Sdclang
+git clone https://gitlab.com/VoidUI/snapdragon-clang.git prebuilts/clang/host/linux-x86/clang-sdclang
 echo 'Completed, Now proceeding to lunch'
