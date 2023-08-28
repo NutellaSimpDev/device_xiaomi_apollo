2901dd61f2ed52b5415efaf63ec2f2ee9275e0fc
NutellaDev <133258835+NutellaSimpDev@users.noreply.github.com>
2023-08-28 09:23:03 -0500

    apollo: Adding ViperFX

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,18 +1,22 @@
 echo 'Starting to clone stuffs needed for your device'
-echo 'Cloning Common Tree [1/4]'
+echo 'Cloning Common Tree [1/5]'
 # Sweet-Common Tree
 git clone https://github.com/NutellaSimpDev/device_xiaomi_sm8250-common.git -b skyline device/xiaomi/sm8250-common
 
-echo 'Cloning Vendor tree [2/4]'
+echo 'Cloning Vendor tree [2/5]'
 # Device Vendor Tree
 git clone https://github.com/NutellaSimpDev/vendor_xiaomi_apollo.git -b skyline vendor/xiaomi
 
-echo 'Cloning Kernel tree [3/4]'
+echo 'Cloning Kernel tree [3/5]'
 # Kernel Tree
 git clone https://github.com/VoidUI-Devices/kernel_xiaomi_sm8250.git -b aosp-13 kernel/xiaomi/sm8250
 
 echo 'Cloning CamGo [4/5]'
 # GcamGo
 rm -rf packages/apps/GCamGOPrebuilt
-git clone https://github.com/ArrowOS-Devices/android_packages_apps_GCamGOPrebuilt -b arrow-13.1 packages/apps/GCamGOPrebuilt
+git clone https://github.com/Evolution-X/packages_apps_GCamGOPrebuilt.git packages/apps/GCamGOPrebuilt
+
+echo 'Cloning ViperFX [5/5]'
+# Kernel Tree
+git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX.git -b v4a packages/apps/ViPER4AndroidFX
 echo 'Completed, Now proceeding to lunch'
