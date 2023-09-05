9165c1c9be9ab4bfa551027b5c3fd7ab4f57af6b
NutellaDev <eduardocayovilca@gmail.com>
2023-09-04 21:36:45 -0500

    apollo: Add ViperFX

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,20 +1,19 @@
 echo 'Starting to clone stuffs needed for your device'
-echo 'Cloning Common Tree [1/4]'
+echo 'Cloning Common Tree [1/5]'
 # Sweet-Common Tree
 git clone https://github.com/NutellaSimpDev/device_xiaomi_sm8250-common.git -b skyline device/xiaomi/sm8250-common
 
-echo 'Cloning Vendor tree [2/4]'
+echo 'Cloning Vendor tree [2/5]'
 # Device Vendor Tree
 git clone https://github.com/NutellaSimpDev/vendor_xiaomi_apollo.git -b skyline vendor/xiaomi
 
-echo 'Cloning Kernel tree [3/4]'
+echo 'Cloning Kernel tree [3/5]'
 # Kernel Tree
 git clone https://github.com/VoidUI-Devices/kernel_xiaomi_sm8250.git -b aosp-13 kernel/xiaomi/sm8250
 
 echo 'Cloning Miuicamera [4/5]'
 # Memecam
-git clone https://gitlab.com/NutellaSimpDev/vendor_xiaomi_apollo-miuicamera vendor/xiaomi/apollo-miuicamera
-
+git clone https://github.com/NutellaSimpDev/vendor_xiaomi_apollo-miuicamera.git vendor/xiaomi/apollo-miuicamera
 echo 'Cloning ViperFX [5/5]'
 # Kernel Tree
 git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX.git -b v4a packages/apps/ViPER4AndroidFX
